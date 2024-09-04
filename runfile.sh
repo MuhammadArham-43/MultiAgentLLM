a8a1b614473f1a73f8831eb6c50662bba68591eb
Muhammad Arham <iqbalarham43@gmail.com>
2024-09-05 00:15:37 +0200

    Bash file to setup and execute servers

---
+++ runfile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+VENV_DIR="venv"
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Creating a new virutal environment venv for Python"
+    python3 -m venv venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+else source venv/bin/activate
+fi
+ollama serve
+set -a
+source .env
+nohup streamlit run client.py &
+python3 main.py
